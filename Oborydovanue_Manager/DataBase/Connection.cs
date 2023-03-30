fbad4a3994fab3eeb147e870b76ba8658b105516
Арсений Макаров <makki09092004@hotmail.com>
2023-03-30 08:10:19 +0300

    Добавление менеджера

---
+++ Oborydovanue_Manager/DataBase/Connection.cs
@@ -0,0 +1,15 @@
+﻿using Oborydovanue_Manager.DataBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oborydovanue_Deliverier.DataBase
+{
+    internal class Connection
+    {
+        public static ArendaEntities db = new ArendaEntities();
+    }
+}
+
