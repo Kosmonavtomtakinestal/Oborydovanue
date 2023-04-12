da06252f9a0e4cc1d4e5e49c6a319e71ce715c49
Kosmonavtomtakinestal <kosmonavt.tak.i.ne.stal@gmail.com>
2023-04-12 08:14:25 +0300

    Работник пункта выдачи

---
+++ Oborydovanue_StoreEmployee/Pages/AuthPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Oborydovanue_StoreEmployee.DataBase;
+
+namespace Oborydovanue_StoreEmployee.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthPage.xaml
+    /// </summary>
+    public partial class AuthPage : Page
+    {
+        public AuthPage()
+        {
+            InitializeComponent();
+        }
+
+        private void AuthBTN_Click(object sender, RoutedEventArgs e)
+        {
+            Auth();
+        }
+
+        private void Auth()
+        {
+            if (LoginTb.Text.Trim() == "" || PasswordTb.Text.Trim() == "")
+            {
+                MessageBox.Show("Заполните все поля", "Уведомления", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                SaveSomeData.storeEmployee = Connection.db.StoreEmployee.ToList().Find(x => x.Login == LoginTb.Text.Trim() && x.Password == PasswordTb.Text.Trim());
+                if (SaveSomeData.storeEmployee != null)
+                {
+                    SaveSomeData.main.MainFrame.Navigate(new ProductPage());
+                }
+                else
+                {
+                    MessageBox.Show("Не верно введены данные", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+            }
+        }
+
+        private void Page_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                Auth();
+        }
+    }
+}
---
+++ Oborydovanue_StoreEmployee/Pages/ProductPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Oborydovanue_StoreEmployee.DataBase;
+using Oborydovanue_StoreEmployee.Pages.PageItems;
+
+namespace Oborydovanue_StoreEmployee.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ProductPage.xaml
+    /// </summary>
+    public partial class ProductPage : Page
+    {
+        public ProductPage()
+        {
+            InitializeComponent();
+            SaveSomeData.productPage = this;
+            SmallFrame.Navigate(new ProductListPage());
+            List<string> sortList = new List<string>() { "по умолчанию", "Цена ↑", "Цена ↓" };
+            SortCB.ItemsSource = sortList;
+        }
+        private void Refresh()
+        {
+            IEnumerable<Stock> filter = Connection.db.Stock.Where(x => x.IdPoinOfIssue == SaveSomeData.storeEmployee.IdpoinOfIssue);
+
+            if (SortCB.SelectedIndex > 0)
+            {
+                if (SortCB.SelectedIndex == 1)
+                    filter = filter.OrderBy(x => x.Product.Price);
+                else if (SortCB.SelectedIndex == 2)
+                    filter = filter.OrderByDescending(x => x.Product.Price);
+            }
+
+            if (SearchTB.Text.Length > 0)
+            {
+                filter = filter.Where(x => x.Product.Name.ToLower().Contains(SearchTB.Text.ToLower()) || x.Product.Discription.ToLower().Contains(SearchTB.Text.ToLower()));
+            }
+
+            SaveSomeData.productListPage.ProductList.ItemsSource = filter.ToList();
+        }
+
+        private void SearchTB_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Refresh();
+        }
+
+        private void SortCB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Refresh();
+        }
+    }
+}
