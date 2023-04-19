2faa85b0cd4c3e0697f957959ebdbd461e3e53fb
Kosmonavtomtakinestal <kosmonavt.tak.i.ne.stal@gmail.com>
2023-04-19 12:04:17 +0300

    работник пункта выдачи и маленькая ошибка коиента

---
+++ Oborydovanue_StoreEmployee/DataBase/PartialClass/Stock.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oborydovanue_StoreEmployee.DataBase
+{
+    partial class Stock
+    {
+        public int CountInRent
+        {
+            get
+            {
+                int count = 0;
+                foreach (var item in Rent)
+                {
+                    if (item.Stock.IdPoinOfIssue == IdPoinOfIssue && item.Stock.IdProduct == IdProduct && item.IdStoreEmployee != null && item.Returned == false)
+                    {
+                        count ++;
+                    }
+                }
+                return count;
+            }
+        }
+    }
+}
