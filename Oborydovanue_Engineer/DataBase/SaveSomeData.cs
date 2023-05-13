5510431be256e00ec6f08c977ca5bdd8a22e9299
Kosmonavtomtakinestal <kosmonavt.tak.i.ne.stal@gmail.com>
2023-05-13 23:30:59 +0300

    Инженер

---
+++ Oborydovanue_Engineer/DataBase/SaveSomeData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oborydovanue_Engineer.DataBase
+{
+    internal class SaveSomeData
+    {
+        public static Engineer engineer;
+    }
+}
